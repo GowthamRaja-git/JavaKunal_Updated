bf3bdd24c45b427f51292f34de96177bfbf57330
Gowtham Raja <99356838+GowthamRaja-git@users.noreply.github.com>
2023-07-18 21:03:09 +0530

    Add files via upload

---
+++ java kunal/Arr.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Arr {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // int[] arr = new int[5];
+        // System.out.println("Enter the 5 numbers : ");
+
+        // for(int i=0; i<arr.length; i++){
+            
+        //     arr[i] =  in.nextInt();
+        //     System.out.print(arr[i] + " ");
+
+
+        // }
+
+        String[] str = new String[4];
+        
+        for (int i = 0; i < str.length; i++){
+            str[i] = in.next();
+            System.out.println(str[i]+ " ");
+
+        }
+
+        
+    }
+    
+}
